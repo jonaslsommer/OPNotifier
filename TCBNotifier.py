63f892030ac211fb03e2e2bb88b5742758775aa4
jonaslsommer <jonaslsommer@gmail.com>
2026-01-15 19:01:01 +0100

    Update TCBNotifier.py

--- TCBNotifier.py
+++ TCBNotifier.py
@@ -5,28 +5,15 @@ from PIL import Image
 from io import BytesIO
 from numpy import array,sum
 
-async def check_website(url_name, word):
-    try:
-        response = requests.get(url=url_name)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            component = soup.select_one("body > main > div.overflow-hidden > div.container.mx-auto.text-center.p-4 > h1")
-            if word in component.get_text():
-                return component.get_text()[-4:]
-            else:
-                return None
-        else:
-            return None
-    except requests.exceptions.RequestException as e:
-        return None
-
 async def check_cover(url_name, temp, number):
     try:
         response = requests.get(url=url_name)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
             image = soup.find('img', alt=lambda x: x and f'One Piece  Chapter {temp} Page {number}' in x)
+            print(f'One Piece  Chapter {temp} Page {number}')
             if image:
+                print("found")
                 return image['src']
             else:
                 return None
@@ -35,7 +22,23 @@ async def check_cover(url_name, temp, number):
     except requests.exceptions.RequestException as e:
         return None
 
-async def check_continuous_chapters(known_chapter_url): 
+async def check_button(url_base,url_name):
+    try:
+        response = requests.get(url=url_name)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            component = soup.select_one("body > main > div.overflow-hidden > div.container.mx-auto.text-center.p-4 > div.flex.items-center.justify-center.my-6.gap-2.text-sm.font-bold > a:nth-child(3)")
+            if component:
+                print("next rec on" + str(url_base + str(component.get("href")[10:14])))
+                return url_base + str(component.get("href")[10:14]),str(component.get("href")[-4:])
+            else:
+                return None,None
+        else:
+            return None,None
+    except requests.exceptions.RequestException as e:
+        return None,None
+
+async def check_contiuous_button(known_chapter_url, number = 0):
     if len(known_chapter_url) > 60:
         subdomain_number = int(known_chapter_url[:-23][-4:])
         chapter_number = subdomain_number
@@ -44,30 +47,29 @@ async def check_continuous_chapters(known_chapter_url):
         subdomain_number = int(known_chapter_url[-4:])
         chapter_number = subdomain_number
         url_base = known_chapter_url[:41]
-    count = 0
     cover = None
-    while count <= 20:
-        subdomain_number += 1
-        url_name = url_base + str(subdomain_number)
-        #print(url_name)
-        temp = await check_website(url_name, 'One Piece  - Chapter')
-        if(temp):
-            cover = await check_cover(url_name,temp,1)
-            #print(temp,'exists neu:',url_name,'alt:',known_chapter_url)
-            known_chapter_url = url_name
-            chapter_number = temp
-            count = 0
-        count += 1
-        sleep(0.2)
-    i = 1
-    while cover != None:
-        if  not await image_color(cover):
-            break
+    url_name = url_base + str(subdomain_number)
+    while True:
+        _url_name, _chapter_number = await check_button(url_base,url_name)
+        if _url_name:
+            url_name = _url_name
+            chapter_number = _chapter_number
         else:
-            i+=1
-            cover = await check_cover(known_chapter_url,chapter_number,i)
-        if i >= 3: break
-    return [known_chapter_url,chapter_number,cover]
+            break
+    print(url_name,chapter_number)
+    if url_name == url_base + str(subdomain_number):
+        return [url_name,chapter_number,cover]
+    else:
+        i = 1
+        cover = await check_cover(url_name,chapter_number,1)
+        while cover != None:
+            if  not await image_color(cover):
+                break
+            else:
+                i+=1
+                cover = await check_cover(url_name,chapter_number,i)
+            if i >= 3: break
+        return [url_name,chapter_number,cover]
 
 async def image_color(picture_url):
     headers = {
